0e3a6e20936d17b81b22dd8c9f3356ae0600b45b
Alexander Picken <pickeale@students.zhaw.ch>
2021-10-21 16:18:14 +0200

    SW 5 abgeschlossen

--- processapplication/src/main/java/ch/zhaw/gpi/twitterreviewprocessapplication/TwitterReviewProcessApplication.java
+++ processapplication/src/main/java/ch/zhaw/gpi/twitterreviewprocessapplication/TwitterReviewProcessApplication.java
@@ -9,22 +9,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * ausführt. Basis ist das SpringBoot-Framework, welches von Camunda erweitert
  * wurde.
  *
- * Zusammengefasst werden dabei folgende Schritte durchlaufen: 
- * 1. Tomcat initialisieren 2. Camunda REST API aktivieren 3.
- * Camunda Job Executor initialisieren 4. Camunda Process Engine inklusive
- * Datenbank initialisieren gemäss application.properties
- * 5. Sofern noch nicht vorhanden, den Demo-Admin-User erstellen gemäss
- * application.properties 6. Die Webapps (Tasklist, Admin, Cockpit) deployen 7.
- * Gefundene Prozesse (z.B. ExampleProcess) deployen 8. Alle Komponenten starten
- * => unter localhost:8080 ist Tomcat erreichbar *
+ * Zusammengefasst werden dabei folgende Schritte durchlaufen: 1. Tomcat
+ * initialisieren 2. Camunda REST API aktivieren 3. Camunda Job Executor
+ * initialisieren 4. Camunda Process Engine inklusive Datenbank initialisieren
+ * gemäss application.properties 5. Sofern noch nicht vorhanden, den
+ * Demo-Admin-User erstellen gemäss application.properties 6. Die Webapps
+ * (Tasklist, Admin, Cockpit) deployen 7. Gefundene Prozesse (z.B.
+ * ExampleProcess) deployen 8. Alle Komponenten starten => unter localhost:8080
+ * ist Tomcat erreichbar *
  */
 @SpringBootApplication
 @EnableProcessApplication
 public class TwitterReviewProcessApplication {
 
     /**
-     * Haupt-Methode, welche beim Run-Befehl eine
-     * Camunda-Prozessapplikation erstellt.
+     * Haupt-Methode, welche beim Run-Befehl eine Camunda-Prozessapplikation
+     * erstellt.
      *
      * @param args
      */
